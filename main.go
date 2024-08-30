24d1019d672ae1bf70599b68e7389c26216debf2
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-08-30 21:38:50 +0300

    feat: cobra init

--- main.go
+++ main.go
@@ -2,35 +2,33 @@ package main
 
 import (
 	_ "github.com/lib/pq"
-	"orm/entities"
-	"orm/lib/client"
-	"orm/lib/config"
-	"orm/lib/scheme"
+	"orm/cmd"
 )
 
 func main() {
-	userTableSchema := scheme.TableScheme{
-		Name: "users",
-		Fields: []scheme.Field{
-			{Name: "id", Type: "integer"},
-			{Name: "name", Type: "varchar(50)"},
-			{Name: "email", Type: "varchar(100)"},
-			{Name: "password", Type: "varchar(100)"},
-		},
-	}
-
-	ormConfig := config.ORMConfig{
-		DbDriver: "postgres",
-		Host:     "localhost",
-		Port:     15432,
-		User:     "postgres",
-		Password: "root",
-		DbName:   "orm",
-		SSLMode:  false,
-		Tables: []scheme.TableScheme{
-			userTableSchema,
-		},
-	}
+	cmd.Execute()
+	//userTableSchema := scheme.TableScheme{
+	//	Name: "users",
+	//	Fields: []scheme.Field{
+	//		{Name: "id", Type: "integer"},
+	//		{Name: "name", Type: "varchar(50)"},
+	//		{Name: "email", Type: "varchar(100)"},
+	//		{Name: "password", Type: "varchar(100)"},
+	//	},
+	//}
+	//
+	//ormConfig := config.ORMConfig{
+	//	DbDriver: "postgres",
+	//	Host:     "localhost",
+	//	Port:     15432,
+	//	User:     "postgres",
+	//	Password: "root",
+	//	DbName:   "orm",
+	//	SSLMode:  false,
+	//	Tables: []scheme.TableScheme{
+	//		userTableSchema,
+	//	},
+	//}
 
 	//ormConfig := config.ORMConfig{
 	//	DbDriver: "sqlite",
@@ -45,15 +43,15 @@ func main() {
 	//	},
 	//}
 
-	db := client.CreateClient(ormConfig)
-
-	user := &entities.User{
-		Model: entities.Model{},
-		Name:  "Name",
-		Email: "Email",
-	}
-
-	db.Create(user)
+	//db := client.CreateClient(ormConfig)
+	//
+	//user := &entities.User{
+	//	Model: entities.Model{},
+	//	Name:  "Name",
+	//	Email: "Email",
+	//}
+	//
+	//db.Create(user)
 
 	//err := schema.createTable(db)
 	//if err != nil {
