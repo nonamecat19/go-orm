35ee1eddb685ff6e1219a5347875bfb6d7955eb0
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-09-14 21:04:45 +0300

    refactor(global): decouple cli module

---
+++ cli/go.mod
@@ -0,0 +1,3 @@
+module github.com/nonamecat19/go-orm/cli
+
+go 1.23.1
--- cli/main.go
+++ cli/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "github.com/lib/pq"
-	"github.com/nonamecat19/go-orm/orm/cmd"
+	"github.com/nonamecat19/go-orm/cli/cmd"
 )
 
 func main() {
