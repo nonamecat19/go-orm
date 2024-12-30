27dbb4b9d0421ee1a874e98963055b0d16b6c6e7
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2024-12-30 19:48:15 +0200

    feat(app): integrate make for cli testing

--- cli/cmd/migrationCreate.go
+++ cli/cmd/migrationCreate.go
@@ -26,7 +26,6 @@ var migrationCreateCmd = &cobra.Command{
 		}
 
 		println("Creating migration", name)
-
 	},
 }
 
--- core/lib/config/yaml.go
+++ core/lib/config/yaml.go
@@ -1,11 +1,13 @@
 package config
 
 import (
+	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 )
 
 func ParseYamlConfig(path string) (*ORMConfigYaml, error) {
+	fmt.Print(path)
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, err
