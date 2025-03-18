cf2693298fb3653d3d17d1bd893ef2d75d8a94ea
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-03-18 19:08:54 +0200

    feat: update test users entity

--- app/entities/user.go
+++ app/entities/user.go
@@ -4,8 +4,11 @@ import "github.com/nonamecat19/go-orm/core/lib/entities"
 
 type User struct {
 	entities.Model
-	Name  string `db:"name" json:"name"`
-	Email string `db:"email" json:"email"`
+	Name      string     `db:"name" json:"name"`
+	Email     string     `db:"email" json:"email"`
+	Gender    string     `db:"gender" json:"gender"`
+	Orders    []Order    `db:"orders" json:"orders"`
+	Favorites []Favorite `db:"favorites" json:"favorites"`
 }
 
 func (user User) Info() string {
